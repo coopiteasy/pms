b093acdbfe2e61bed78058104eecede367e19269
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-07-19 11:43:48 +0200

    [ADD] pms_website_sale

---
+++ pms_website_sale/__manifest__.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "PMS Website Sale",
+    "summary": """
+        Book and pay for hotel reservations online.""",
+    "version": "14.0.1.0.0",
+    "category": "E-commerce",
+    "website": "https://github.com/OCA/pms",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [],
+    "excludes": [],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
